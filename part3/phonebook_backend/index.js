cb5da2720d7ba2a49f42d95c9651837eb626c0b7
morten stoltenberg <mortenstolt@gmail.com>
2025-05-08 12:04:06 +0200

    3.21

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -160,7 +160,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
